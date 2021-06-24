70ab85a91cee0bfda8ea5e4721fb41f64263db5b
vladkage <hokage.vlados@gmail.com>
2021-06-24 23:03:07 +0300

    Timezones

--- App/TimezoneCore.php
+++ App/TimezoneCore.php
@@ -1,6 +1,24 @@
 <?php
 
-class TimezoneCore
+namespace App;
+
+use App\Timezones\Interfaces\TimezoneInterface;
+
+class TimezoneCore implements TimezoneInterface
 {
-    
+
+    public function setTimezoneName($timezone): TimezoneInterface
+    {
+        return $timezone;
+    }
+
+    public function selectTimezoneUSA()
+    {
+
+    }
+
+    public function selectTimezoneCanada()
+    {
+        
+    }
 }
